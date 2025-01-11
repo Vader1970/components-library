92376c5ce7bc6c117f30b52f96ab5e58605767c7
Vader1970 <daniel.wilkey@gmail.com>
2025-01-11 15:44:10 +1300

    useTransform Issues on Deployment

--- src/app/(pages)/banners/page.tsx
+++ src/app/(pages)/banners/page.tsx
@@ -8,10 +8,10 @@ import { Banner1 } from "@/components/Banner1";
 import { Banner10 } from "@/components/Banner10";
 import { Banner11 } from "@/components/Banner11";
 import { Banner12 } from "@/components/Banner12";
-import { Banner13 } from "@/components/Banner13";
-import { Banner14 } from "@/components/Banner14";
+// import { Banner13 } from "@/components/Banner13";
+// import { Banner14 } from "@/components/Banner14";
 import { Banner15 } from "@/components/Banner15";
-import { Banner16 } from "@/components/Banner16";
+// import { Banner16 } from "@/components/Banner16";
 import { Banner2 } from "@/components/Banner2";
 import { Banner3 } from "@/components/Banner3";
 import { Banner4 } from "@/components/Banner4";
@@ -34,10 +34,10 @@ const bannerComponents = [
   { component: Banner10, title: "Banner 10" },
   { component: Banner11, title: "Banner 11" },
   { component: Banner12, title: "Banner 12" },
-  { component: Banner13, title: "Banner 13" },
-  { component: Banner14, title: "Banner 14" },
+  // { component: Banner13, title: "Banner 13" },
+  // { component: Banner14, title: "Banner 14" },
   { component: Banner15, title: "Banner 15" },
-  { component: Banner16, title: "Banner 16" },
+  // { component: Banner16, title: "Banner 16" },
 
   // Add more banners as necessary
 ];
@@ -54,39 +54,34 @@ const BannersPage = () => {
 
   return (
     <div>
-      <h1 className="text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl">
+      <h1 className='text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl'>
         Banners
       </h1>
 
       {/* Search Section */}
-      <Search
-        placeholder="Search for a component (e.g., Banner 9)"
-        onSearch={handleSearch}
-      />
+      <Search placeholder='Search for a component (e.g., Banner 9)' onSearch={handleSearch} />
 
       <section>
-        {bannerComponents.map(
-          ({ component: BannerComponent, title }, index) => (
-            <div key={index} id={title.toLowerCase().replace(/\s+/g, "")}>
-              {/* Render the title as an h2 element */}
-              <h2 className="bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center">
-                {title}
-                <a
-                  href={`/view-code/${title.replace(/\s+/g, "")}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
-                >
-                  View Code
-                </a>
-              </h2>
-              <div className="py-10">
-                {/* Render the component */}
-                <BannerComponent />
-              </div>
+        {bannerComponents.map(({ component: BannerComponent, title }, index) => (
+          <div key={index} id={title.toLowerCase().replace(/\s+/g, "")}>
+            {/* Render the title as an h2 element */}
+            <h2 className='bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center'>
+              {title}
+              <a
+                href={`/view-code/${title.replace(/\s+/g, "")}`}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200'
+              >
+                View Code
+              </a>
+            </h2>
+            <div className='py-10'>
+              {/* Render the component */}
+              <BannerComponent />
             </div>
-          )
-        )}
+          </div>
+        ))}
       </section>
       <ScrollUp />
     </div>
