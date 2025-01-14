8e60a789a4d14801edc8148b51e7fa8653422696
Vader1970 <daniel.wilkey@gmail.com>
2025-01-14 20:19:01 +1300

    changed naming of titles for view

--- src/app/(pages)/faqs/page.tsx
+++ src/app/(pages)/faqs/page.tsx
@@ -20,20 +20,20 @@ import { Faq10 } from "@/components/Faq10";
 import { Faq12 } from "@/components/Faq12";
 
 const faqComponents = [
-  { component: Faq1, title: "FAQ 1" },
-  { component: Faq2, title: "FAQ 2" },
-  { component: Faq3, title: "FAQ 3" },
-  { component: Faq4, title: "FAQ 4" },
-  { component: Faq5, title: "FAQ 5" },
-  { component: Faq6, title: "FAQ 6" },
-  { component: Faq7, title: "FAQ 7" },
-  { component: Faq8, title: "FAQ 8" },
-  { component: Faq9, title: "FAQ 9" },
-  { component: Faq10, title: "FAQ 10" },
-  { component: Faq11, title: "FAQ 11" },
-  { component: Faq12, title: "FAQ 12" },
-  { component: Faq13, title: "FAQ 13" },
-  { component: Faq14, title: "FAQ 14" },
+  { component: Faq1, title: "Faq 1" },
+  { component: Faq2, title: "Faq 2" },
+  { component: Faq3, title: "Faq 3" },
+  { component: Faq4, title: "Faq 4" },
+  { component: Faq5, title: "Faq 5" },
+  { component: Faq6, title: "Faq 6" },
+  { component: Faq7, title: "Faq 7" },
+  { component: Faq8, title: "Faq 8" },
+  { component: Faq9, title: "Faq 9" },
+  { component: Faq10, title: "Faq 10" },
+  { component: Faq11, title: "Faq 11" },
+  { component: Faq12, title: "Faq 12" },
+  { component: Faq13, title: "Faq 13" },
+  { component: Faq14, title: "Faq 14" },
 
   // Add more FAQs as necessary
 ];
@@ -50,27 +50,22 @@ const FaqsPage = () => {
 
   return (
     <div>
-      <h1 className="text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl">
-        FAQS
-      </h1>
+      <h1 className='text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl'>FAQS</h1>
 
       {/* Search Section */}
-      <Search
-        placeholder="Search for a component (e.g., Faq 9)"
-        onSearch={handleSearch}
-      />
+      <Search placeholder='Search for a component (e.g., Faq 9)' onSearch={handleSearch} />
 
-      <section className="mb-4">
+      <section className='mb-4'>
         {faqComponents.map(({ component: FaqComponent, title }, index) => (
           <div key={index} id={title.toLowerCase().replace(/\s+/g, "")}>
             {/* Render the title as an h2 element */}
-            <h2 className="bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center">
+            <h2 className='bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center'>
               {title}
               <a
                 href={`/view-code/${title.replace(/\s+/g, "")}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200'
               >
                 View Code
               </a>
--- src/app/(pages)/logins/page.tsx
+++ src/app/(pages)/logins/page.tsx
@@ -14,14 +14,14 @@ import { Login7 } from "@/components/Login7";
 import { Login8 } from "@/components/LogIn8";
 
 const loginComponents = [
-  { component: Login1, title: "Log In 1" },
-  { component: Login2, title: "Log In 2" },
-  { component: Login3, title: "Log In 3" },
-  { component: Login4, title: "Log In 4" },
-  { component: Login5, title: "Log In 5" },
-  { component: Login6, title: "Log In 6" },
-  { component: Login7, title: "Log In 7" },
-  { component: Login8, title: "Log In 8" },
+  { component: Login1, title: "Log in 1" },
+  { component: Login2, title: "Log in 2" },
+  { component: Login3, title: "Log in 3" },
+  { component: Login4, title: "Log in 4" },
+  { component: Login5, title: "Log in 5" },
+  { component: Login6, title: "Log in 6" },
+  { component: Login7, title: "Log in 7" },
+  { component: Login8, title: "Log in 8" },
 
   // Add more logins as necessary
 ];
@@ -38,27 +38,24 @@ const LogInsPage = () => {
 
   return (
     <div>
-      <h1 className="text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl">
+      <h1 className='text-white bg-black text-4xl text-center tracking-tighter font-bold border-b py-4 sm:5xl'>
         Log Ins
       </h1>
 
       {/* Search Section */}
-      <Search
-        placeholder="Search for a component (e.g., Log In 7)"
-        onSearch={handleSearch}
-      />
+      <Search placeholder='Search for a component (e.g., Log In 7)' onSearch={handleSearch} />
 
-      <section className="mb-4">
+      <section className='mb-4'>
         {loginComponents.map(({ component: LogInComponent, title }, index) => (
           <div key={index} id={title.toLowerCase().replace(/\s+/g, "")}>
             {/* Render the title as an h2 element */}
-            <h2 className="bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center">
+            <h2 className='bg-black text-white text-base sm:text-2xl font-bold text-center py-10 px-[5%] flex justify-between items-center'>
               {title}
               <a
                 href={`/view-code/${title.replace(/\s+/g, "")}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='bg-blue-500 text-white text-base px-4 py-2 rounded hover:bg-blue-600 transition duration-200'
               >
                 View Code
               </a>
